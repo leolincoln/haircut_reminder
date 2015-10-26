8af7d463deb4c86a20f464237c9eeeb54e17a114
liu liu <leoliu@dhcp-10-105-41-238.wireless.northwestern.private>
2015-10-26 16:43:46 -0500

    original commit

---
+++ __init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 06 12:15:07 2015
+
+@author: lliu5
+This module is for extracting, cleaning and filtering the data points from physitian notes provided from Northwestern medicine Datawarehouse
+"""
+
---
+++ api.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 06 12:20:59 2015
+
+@author: liuliu
+"""
+from flask import Flask,render_template
+
+import json
+import traceback
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    '''
+    Returns: 
+        render home.html. 
+    '''
+    files =[]
+    return render_template('home.html',files = files)
+
+@app.route('/houseclean')
+def confidence():
+    '''
+    returns:
+        render houseclean.html
+    '''
+    return render_template('houseclean.html')
+
+
+@app.route('/anniversary')
+def cleaner():
+    return render_template('anniversary.html')
+
+if __name__=='__main__':
+    from send_txt import email
+    email(address='8728068540@tmomail.net',content='this is working right?')
+    app.run(debug=True)
